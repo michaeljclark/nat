c326c3c8f259d3f3428d77b3e9e006e2691daba1
Michael Clark <michaeljclark@mac.com>
2017-08-14 16:14:21 +1200

    Add signed magnitude integer class

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CXX = g++
 CXXFLAGS = -O3 -Wall -std=c++11
 
-all: bin bin/nat-tests bin/nat-demo
+all: bin bin/nat-tests bin/int-tests bin/nat-demo
 
 clean: ; rm -fr bin
 
@@ -11,4 +11,7 @@ bin/nat-tests: src/nat-tests.cc src/nat.h
 	$(CXX) $(CXXFLAGS) -o $@ $<
 
 bin/nat-demo: src/nat-demo.cc src/nat.h
+	$(CXX) $(CXXFLAGS) -o $@ $<
+
+bin/int-tests: src/int-tests.cc src/int.h src/nat.h
 	$(CXX) $(CXXFLAGS) -o $@ $<
---
+++ src/int-tests.cc
@@ -0,0 +1,75 @@
+/*
+ * int-tests.cc
+ *
+ * simple test cases for signed integer implementation
+ *
+ * Copyright (C) 2017, Michael Clark <michaeljclark@mac.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <cassert>
+#include <vector>
+#include <string>
+#include <limits>
+#include <algorithm>
+#include <initializer_list>
+
+#include "nat.h"
+#include "int.h"
+
+int main(int argc, char const *argv[])
+{
+    assert(Int(0) == Int(0));
+    assert(Int(1) == Int(1));
+
+    assert(Int(1) != Int(0));
+    assert(Int(1) != Int(-1));
+
+    assert(Int(2) > Int(1));
+    assert(Int(1) > Int(0));
+    assert(Int(1) > Int(-1));
+    assert(Int(-1) > Int(-2));
+
+    assert(Int(1) < Int(2));
+    assert(Int(0) < Int(1));
+    assert(Int(-1) < Int(1));
+    assert(Int(-2) < Int(-1));
+
+    assert(Int(1) + Int(2) == Int(3));
+    assert(Int(3) + Int(-2) == Int(1));
+    assert(Int(1) + Int(-2) == Int(-1));
+    assert(Int(-2) + Int(-1) == Int(-3));
+    assert(Int(-2) + Int(1) == Int(-1));
+    assert(Int(-2) + Int(3) == Int(1));
+
+    assert(Int(1) - Int(2) == Int(-1));
+    assert(Int(3) - Int(-2) == Int(5));
+    assert(Int(1) - Int(-2) == Int(3));
+    assert(Int(-2) - Int(-1) == Int(-1));
+    assert(Int(-2) - Int(1) == Int(-3));
+    assert(Int(-2) - Int(3) == Int(-5));
+
+    assert(Int(1) * Int(2) == Int(2));
+    assert(Int(3) * Int(-2) == Int(-6));
+    assert(Int(1) * Int(-2) == Int(-2));
+    assert(Int(-2) * Int(-1) == Int(2));
+    assert(Int(-2) * Int(1) == Int(-2));
+    assert(Int(-2) * Int(3) == Int(-6));
+}
