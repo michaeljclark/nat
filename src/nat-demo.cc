d3122fa54f7df7184d851a0e572c1a72b4bd1b46
Michael Clark <michaeljclark@mac.com>
2017-08-21 14:16:53 +1200

    Fix indentation in nat-demo.cc

--- src/nat-demo.cc
+++ src/nat-demo.cc
@@ -5,5 +5,5 @@ int main(int argc, char const *argv[])
 {
 	int val = 71, power = 71117;
 	Nat result =  Nat(val).pow(power);
-    printf("%d^%d is %s\n", val, power, result.to_string().c_str());
+	printf("%d^%d is %s\n", val, power, result.to_string().c_str());
 }
